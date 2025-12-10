d2eac00357ff6e8887701b79de17109a282dfe93
NguyenVuAnNhan <annhan0328@gmail.com>
2025-12-10 21:37:37 +0700

    feat: neo4j config

--- neo4j/src/main/java/com/yahoo/yscb/db/neo4j/Neo4jConfig.java
+++ neo4j/src/main/java/com/yahoo/yscb/db/neo4j/Neo4jConfig.java
@@ -1,3 +1,15 @@
 public class Neo4jConfig {
-    
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public Neo4jConfig(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String url() { return url; }
+    public String username() { return username; }
+    public String password() { return password; }
 }
