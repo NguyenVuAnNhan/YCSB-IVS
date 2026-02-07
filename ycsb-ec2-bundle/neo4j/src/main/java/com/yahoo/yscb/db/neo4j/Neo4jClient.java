9f32da0ce0c176f5e16bf9fa0fa7d9df91f68461
NguyenVuAnNhan <annhan0328@gmail.com>
2026-02-07 22:51:12 +1100

    feat: improved binding for neo4j

--- ycsb-ec2-bundle/neo4j/src/main/java/com/yahoo/yscb/db/neo4j/Neo4jClient.java
+++ ycsb-ec2-bundle/neo4j/src/main/java/com/yahoo/yscb/db/neo4j/Neo4jClient.java
@@ -11,72 +11,76 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 
+/**
+ * YCSB client binding for Neo4j.
+ */
 public class Neo4jClient extends DB {
 
-    private Neo4jConnection conn;
-    private Neo4jConfig config;
+  private Neo4jConnection conn;
+  private Neo4jConfig config;
 
-    @Override
-    public void init() throws DBException {
-        Properties props = getProperties();
+  @Override
+  public void init() throws DBException {
+    Properties props = getProperties();
 
-        String url = props.getProperty("url");
-        String username = props.getProperty("username");
-        String password = props.getProperty("password");
+    String url = props.getProperty("url");
+    String username = props.getProperty("username");
+    String password = props.getProperty("password");
 
-        config = new Neo4jConfig(url, username, password);
-        conn = new Neo4jConnection(config);
+    config = new Neo4jConfig(url, username, password);
+    conn = new Neo4jConnection(config);
+  }
+
+  @Override
+  public void cleanup() throws DBException {
+    if (conn != null) {
+      conn.close();
     }
+  }
 
-    @Override
-    public void cleanup() throws DBException {
-        if (conn != null) {
-            conn.close();
-        }
+  @Override
+  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+    try {
+      return conn.read(table, key, fields, result);
+    } catch (Exception e) {
+      return Status.ERROR;
     }
+  }
 
-    @Override   
-    public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
-        try {
-            return conn.read(table, key, fields, result);
-        } catch (Exception e) {
-            return Status.ERROR;
-        }
+  @Override
+  public Status update(String table, String key, Map<String, ByteIterator> values) {
+    try {
+      return conn.update(table, key, values);
+    } catch (Exception e) {
+      return Status.ERROR;
     }
+  }
 
-    @Override
-    public Status update(String table, String key, Map<String, ByteIterator> values) {
-        try {
-            return conn.update(table, key, values);
-        } catch (Exception e) {
-            return Status.ERROR;
-        }
+  @Override
+  public Status insert(String table, String key, Map<String, ByteIterator> values) {
+    try {
+      return conn.insert(table, key, values);
+    } catch (Exception e) {
+      return Status.ERROR;
     }
+  }
 
-    @Override
-    public Status insert(String table, String key, Map<String, ByteIterator> values) {
-        try {
-            return conn.insert(table, key, values);
-        } catch (Exception e) {
-            return Status.ERROR;
-        }
+  @Override
+  public Status scan(String table, String startkey, int recordcount, Set<String> fields,
+                     Vector<HashMap<String, ByteIterator>> result) {
+    try {
+      return conn.scan(table, startkey, recordcount, fields, result);
+    } catch (Exception e) {
+      return Status.ERROR;
     }
+  }
 
-    @Override
-    public Status scan(String table, String startkey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-        try {
-            return conn.scan(table, startkey, recordcount, fields, result);
-        } catch (Exception e) {
-            return Status.ERROR;
-        }
+  @Override
+  public Status delete(String table, String key) {
+    try {
+      return conn.delete(table, key);
+    } catch (Exception e) {
+      return Status.ERROR;
     }
-
-    @Override
-    public Status delete(String table, String key) {
-        try {
-            return conn.delete(table, key);
-        } catch (Exception e) {
-            return Status.ERROR;
-        }
-    }
+  }
 }
